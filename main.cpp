f78a3a70b314cd284fddc4c515c825b99177b102
EC2 Default User <ec2-user@ip-172-31-42-115.us-east-2.compute.internal>
2023-08-18 04:30:34 +0000

    added dice rolls

--- main.cpp
+++ main.cpp
@@ -2,19 +2,22 @@
 #include <vector>
 #include <algorithm>
 #include <limits>
+#include <random>
+#include <ctime>
 
 using namespace std;
 
 
 class master{
     public:
-    master(const string name, const int health, const int roll, const int initiativeModifier){
+    master(const string name, const int health, const int roll, const int initiativeModifier, const int armorClass){
         this -> name = name;
         this -> health = health;
         this -> roll = roll;
         this -> initiativeModifier = initiativeModifier;
+        this -> armorClass = armorClass;
     }
-string getname() const{
+string getName() const{
     return name;
     }
 int getHealth() const{
@@ -26,18 +29,32 @@ int getRoll() const{
 int getInitiativeModifier() const{
     return initiativeModifier;
 }
+int getArmorClass() const{
+    return armorClass;
+}
     private:
     string name;
     int health;
     int roll;
     int initiativeModifier;
+    int armorClass;
 };
+
+int basicD20Roll(){
+    int d_20 = 0;
+    mt19937 rng(time(nullptr));
+    uniform_int_distribution<int> dist(1, 20);
+    d_20 = dist(rng);
+    return d_20;
+    
+}
 
 void createCharacter(vector<master>& playerStatHolder){
     string name;
     int health;
     int roll;
     int initiativeModifier;
+    int armorClass;
     string inputs;
     
     cout << endl << endl;
@@ -48,28 +65,151 @@ void createCharacter(vector<master>& playerStatHolder){
     cout << "Please enter player health " << endl;
     cin >> health;
     
-    cout << "Please enter the players initiative roll(before modifers) " << endl;
-    cin >> roll;
+    cout << "rolling a d20 for the player " << endl;
+    roll = basicD20Roll();
     
     cout << "Please enter the players intiative modifier" << endl;
     cin >> initiativeModifier;
     
-    roll = roll + initiativeModifier;
+    cout << "Please enter the players armorClass" << endl;
+    cin >> armorClass;
     
-    playerStatHolder.push_back(master(name,health,roll,initiativeModifier));
+    roll += initiativeModifier;
+    
+    playerStatHolder.push_back(master(name,health,roll,initiativeModifier, armorClass));
     
     cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     
     cout << "if you are done please press d, or press c to add more players!" << endl;
 }
 
+
+int rollHelper(int amountRoll, mt19937& rng, uniform_int_distribution<int>& dist){
+    int rollTotal = 0;
+    int diceRoll = 0;
+    for(int i = 0; i < amountRoll; i++){
+        diceRoll = dist(rng);
+        rollTotal += diceRoll;
+        cout << "total roll: " << rollTotal << endl;
+    }
+    return rollTotal;
+}
+
+int diceRoll() {
+    mt19937 rng(time(nullptr));
+    string diceChoice;
+    int diceConvert;
+    int diceRoll;
+    int amountRoll;
+    int rollTotal = 0;
+    cout << "what type of dice would you like me to roll? ex(d4)" << endl;
+    cin >> diceChoice;
+    
+    diceConvert = stoi(diceChoice.substr(1));
+    
+    uniform_int_distribution<int> dist(1, diceConvert);
+    while (true){
+        diceConvert = stoi(diceChoice.substr(1));
+        switch(diceConvert){
+            case 4:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+           
+            case 6:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            case 8:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            case 10:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            case 12:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            case 20:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            case 100:
+            cout << "How many times shall I roll?" << endl;
+            cin >> amountRoll;
+            rollTotal = rollHelper(amountRoll, rng, dist);
+            
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            break;
+            
+            default:
+            cout << "Invalid dice, please try again" << endl;
+            cin >> diceChoice;
+            continue;
+            
+        }
+        break;
+    }
+    return rollTotal;
+}
+
 void combat(vector<master>& playerStatHolder){
-    cout << "You have begun combat please press d when done, or c to continue when prompted" << endl;
+    bool isInCombat = true;
+    int playerSelect;
+    int diceRoll;
+    int diceModifier;
+    
+    while(isInCombat){
+        cout << "You have begun combat please press d when done, or c to continue when prompted" << endl;
+        
+        for(int i = 0; i < playerStatHolder.size(); i++){
+            cout << (i + 1) << ": " << playerStatHolder.at(i).getName() << endl;
+        }
+        
+        cout << "Who is being attacked?" << endl;
+        cin >> playerSelect;
+        cout << "You have selected " << playerStatHolder.at(playerSelect - 1).getName() << " prepare for combat!" << endl;
+        cout << "Armor class: " << playerStatHolder.at(playerSelect - 1).getArmorClass() << endl;
+        cout << "Rolling a d20 for attack" << endl;
+        int d_20 = basicD20Roll();
+        cout << "Please enter in hit modifier here" << endl;
+        cin >> diceModifier;
+        d_20 += diceModifier;
+        cout << "Total attack roll: " << d_20 << endl;
+        //enter kill statement here
+        isInCombat = false;
+    }
 }
 
 
 
 int main(){
+    diceRoll();
   string inputs = "a";
   vector<master> playerStatHolder;
   vector<int> initiativeHolder;
@@ -96,9 +236,10 @@ cout << "Enter in player info, press d when prompted if done" << endl;
   
   for(int i = 0; i < playerStatHolder.size(); i++){
       cout << '\n';
-      cout << "Name: " << playerStatHolder.at(i).getname() << endl;
+      cout << "Name: " << playerStatHolder.at(i).getName() << endl;
       cout << "health: " << playerStatHolder.at(i).getHealth() << endl;
-      cout << "roll w/ modifier: " << playerStatHolder.at(i).getRoll() << endl << endl;
+      cout << "roll w/ modifier: " << playerStatHolder.at(i).getRoll() << endl;
+      cout << "Armor class: " << playerStatHolder.at(i).getArmorClass() << endl;
   }
  
  cout << "Would you like to begin combat? type y for yes" << endl;
